c9ad2be10f54dffbc8acaa34c75e3d166d5f8eb1
defund <will.austin.wang@gmail.com>
2017-08-27 22:18:20 -0400

    format database

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,26 +1,21 @@
+function newuser() {
+	default = {
+		N: 16384,
+		r: 8,
+		p: 1,
+		length: 32,
+		alphabet: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+				   abcdefghijklmnopqrztuvwxyz\
+				   0123456789!@#$%^&*()'
+	}
+	firebase.database().ref('/users/'+firebase.auth().currentUser.uid+'/settings').set(default);
+}
+
 firebase.auth().onAuthStateChanged(function(user) {
 	menu.curr = '';
 	select.entries = [];
 	if (user) {
 		profile.number = user.phoneNumber;
-		firebase.database().ref("/users/"+user.uid+"/services").on("value", function(snapshot) {
-			var entries = [];
-			var services = snapshot.val();
-			for (s in services) {
-				services[s].id = s;
-				entries.push(services[s]);
-			}
-			select.entries = entries;
-			if (entries) {
-				generate.entry = select.entries[0];
-			} else {
-				generate.entry = null;
-			}
-		});
-		firebase.database().ref("/users/"+user.uid+"/settings").on("value", function(snapshot) {
-			Object.assign(update.response, snapshot.val());
-			Object.assign(create.response, snapshot.val());
-		});
 		firebase.database().ref("/users/"+user.uid).on("value", function(snapshot) {
 			var result = snapshot.val();
 
@@ -38,8 +33,12 @@ firebase.auth().onAuthStateChanged(function(user) {
 			}
 
 			var settings = result.settings;
-			Object.assign(update.response, settings);
-			Object.assign(create.response, settings);
+			if (settings) {
+				Object.assign(update.response, settings);
+				Object.assign(create.response, settings);
+			} else {
+				newuser();
+			}
 
 			profile.raw = JSON.stringify(result);
 			menu.auth = true;
