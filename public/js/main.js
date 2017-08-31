45f6e1d1ceffe2809d750bfc6e679a0b9c3b2f2c
kmh11 <hoppykev@gmail.com>
2017-08-31 18:49:53 -0400

    fix

--- public/js/main.js
+++ public/js/main.js
@@ -30,7 +30,7 @@ var menu = new Vue({
 	el: '#menu',
 	data: {
 		auth: null,
-		tab: location.hash.slice(1)
+		tab: ["home", "archive", "create", "profile", "settings"].indexOf(location.hash.slice(1)) === -1 ? "home" : location.hash.slice(1)
 	}
 });
 
