f34adda4dfb3f3527cc462b613416f4b6268ad95
kmh11 <hoppykev@gmail.com>
2017-10-07 15:20:29 -0400

    don't remove things

--- build.sh
+++ build.sh
@@ -14,7 +14,6 @@ cp -r build/lib web/public/lib
 cd build/web
 firebase deploy --token ${token} --only hosting
 cd ../../
-rm -r build/web
 echo "=== Web deploy complete. ==="
 echo "=== Building mobile app... ==="
 cp -r mobile build/mobile
@@ -25,7 +24,6 @@ cordova platforms add android
 cordova build --release
 cd ../..
 cp build/mobile/platforms/android/build/outputs/apk/android-release-unsigned.apk build/out/keyster.apk
-rm -r build/mobile
 echo "=== Mobile build complete. ==="
 echo "=== Building Electron app... ==="
 cp -r desktop build/desktop
@@ -36,6 +34,6 @@ electron-packager build/desktop --platform linux --arch x64 --out build/src/
 electron-installer-debian --src build/src/Keyster-linux-x64/ --dest build/out/ --arch amd64
 electron-packager build/desktop --platform darwin --arch x64 --out build/src/
 cp -r build/src/Keyster-darwin-x64/Keyster.app build/out/Keyster.app
-rm -r build/desktop build/src
+rm -r build/src
 echo "=== Electron build complete. ==="
 rm -r build/lib build/css
