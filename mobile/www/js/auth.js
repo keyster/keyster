ca032cfa42cf238ad32399b8f690c181444aa018
kmh11 <hoppykev@gmail.com>
2017-10-15 11:31:59 -0400

    offline on mobile, spacing on mobile

--- mobile/www/js/auth.js
+++ mobile/www/js/auth.js
@@ -2,6 +2,10 @@ function redirect() {
 	location.href = "login/index.html"
 }
 
+function offline() {
+	location.href = "offline/index.html"
+}
+
 firebase.auth().onAuthStateChanged(function(user) {
 	if (user) {
 		profile.number = user.phoneNumber;
@@ -13,3 +17,17 @@ firebase.auth().onAuthStateChanged(function(user) {
 		redirect();
 	}
 })
+
+var connection = firebase.database().ref(".info/connected");
+connection.on("value", function(snap) {
+	if (snap.val()) {
+		window.connected = true;
+	} else {
+		window.connected = false;
+		setTimeout(function() {
+			if (!window.connected) {
+				offline();
+			}
+		}, 5000);
+	}
+});
